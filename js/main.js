0692fe722188e4874bcb6afa58090a09d9b2f137
Orest-Programmer <85691769+Orest-Programmer@users.noreply.github.com>
2022-04-23 21:48:46 +0300

    Add files via upload
    add 3

---
+++ js/main.js
@@ -0,0 +1,87 @@
+const player = document.querySelector(".player"),
+musicName = document.querySelector(".name_music"),
+musicSrc = document.querySelector("#music"),
+playPauseBtn = document.querySelector(".play"),
+progressBar = document.querySelector(".progress"),
+progressContainer = document.querySelector(".progress__container"),
+imgPlayBtn = document.querySelector(".img__src");
+
+window.addEventListener("load", ()=>{
+    loadMusic(musicName);
+})
+
+function loadMusic(song){
+    song = musicName.innerHTML;
+    musicSrc.src = `audio/${song}.mp3`
+}
+
+function playMusic(){
+    player.classList.add("paused");
+    musicSrc.play();
+    imgPlayBtn.src = "img/pause.png";
+}
+
+function pausedMusic(){
+    player.classList.remove("paused");
+    musicSrc.pause();
+    imgPlayBtn.src = "img/play.png";
+}
+
+
+playPauseBtn.addEventListener("click", ()=>{
+    const isMusicPaused = player.classList.contains("paused");
+    isMusicPaused ? pausedMusic() : playMusic();
+})
+
+musicSrc.addEventListener("timeupdate", (e)=>{
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+    let progressWidth = (currentTime / duration) * 100;
+    progressBar.style.width =`${progressWidth}%`
+    let musicCurrentTime = document.querySelector(".current"),
+    musicDuration = document.querySelector(".duration");
+    musicSrc.addEventListener("loadeddata", ()=>{
+        
+
+        let audioDuration = musicSrc.duration;
+        let totalMin = Math.floor(audioDuration / 60);
+        let totalSec = Math.floor(audioDuration % 60);
+        if(totalSec < 10){
+            totalSec = `0${totalSec}`;
+        }
+        musicDuration.innerText = `${totalMin}:${totalSec}`;
+
+    });
+
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
+    if(currentSec < 10){
+        currentSec = `0${currentSec}`;
+    }
+    musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+});
+
+progressContainer.addEventListener("click", (e)=>{
+    let progressWidthval = progressContainer.clientWidth;
+    let clickedOffSet = e.offsetX;
+    let songDuration = musicSrc.duration;
+
+    musicSrc.currentTime = (clickedOffSet / progressWidthval) * songDuration;
+});
+
+var modal = document.getElementById("video");
+
+var btn = document.getElementById("btn_modal_window");
+
+var close = document.getElementsByClassName("close_modal_window")[0];
+
+btn.onclick = function(){
+    modal.style.display = "block";
+}
+
+
+
+close.onclick = function(){
+    modal.style.display = "none";
+}
+
